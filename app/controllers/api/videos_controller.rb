cf342eeadccc4ca03b0cae3a5dee0de2d0a96075
Tony_Huang <luck938938@hotmail.com>
2019-05-03 23:18:41 -0400

    clean up code

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,21 +1,3 @@
 class Api::VideosController < ApplicationController
-  # def index
-  #   @videos =  Video.all
-  #   render json: @videos
-  # end
 
-  # def show
-  #   @video = Video.find(params[:id])
-  #   render json: @video
-  # end
-
-#   def create
-#     @video=Video.create(video_params)
-#     render json: @video 
-# end
-
-# private
-# def video_params
-#     require(:video).permit(:url, :description, :title)
-# end
 end
