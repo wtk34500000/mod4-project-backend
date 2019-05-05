5378a1bacc834b23659f8b1122268283a4ae0455
Tony_Huang <luck938938@hotmail.com>
2019-05-04 23:06:09 -0400

    fix bug

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,8 +1,7 @@
 class Api::AuthController < ApplicationController
+  
     def create
-
       @user = User.find_by(user_email: params[:user][:email])
-    
       if @user && @user.authenticate(params[:user][:password])
         token = JWT.encode({ user_id: @user.id }, "secret")
         render json: { user: @user, jwt: token }, status: :accepted
@@ -16,6 +15,6 @@ class Api::AuthController < ApplicationController
       id = JWT.decode(jwt, "secret")[0]["user_id"]
       @user = User.find(id)
       render json: {user: @user}
-  
     end
+
   end
